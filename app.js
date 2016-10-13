c05bf815ed580700ecef53b2adaf0605735bdccd
Gabe Rodriguez <grodriguez@grodriguez-macbookpro3.roam.corp.google.com>
2016-10-13 15:22:47 +0200

    fixed bug

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
+    qs: { access_token: process.env.pageAccess },
     method: 'POST',
     json: messageData
 
