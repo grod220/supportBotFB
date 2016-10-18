6bfaea22878bbdf4d8cf385a3979c96a95d5cf98
Gabe Rodriguez <grodriguez@grodriguez-macbookpro3.roam.corp.google.com>
2016-10-18 12:02:55 +0200

    test

--- app.js
+++ app.js
@@ -103,30 +103,30 @@ const findOrCreateSession = (fbid) => {
 
 // Our bot actions
 const actions = {
-  send({sessionId}, {text}) {
-    console.log('THIS IS THE SESSION ID?????' + sessionId);
+  send(request, {text}) {
+    console.log('THIS IS THE something else? ID?????' + request);
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
-    const recipientId = sessions[sessionId].fbid;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      // We return a promise to let our bot know when we're done sending
-      return fbMessage(recipientId, text)
-      .then(() => null)
-      .catch((err) => {
-        console.error(
-          'Oops! An error occurred while forwarding the response to',
-          recipientId,
-          ':',
-          err.stack || err
-        );
-      });
-    } else {
-      console.error('Oops! Couldn\'t find user for session:', sessionId);
-      // Giving the wheel back to our bot
-      return Promise.resolve()
-    }
+    // const recipientId = sessions[sessionId].fbid;
+    // if (recipientId) {
+    //   // Yay, we found our recipient!
+    //   // Let's forward our bot response to her.
+    //   // We return a promise to let our bot know when we're done sending
+    //   return fbMessage(recipientId, text)
+    //   .then(() => null)
+    //   .catch((err) => {
+    //     console.error(
+    //       'Oops! An error occurred while forwarding the response to',
+    //       recipientId,
+    //       ':',
+    //       err.stack || err
+    //     );
+    //   });
+    // } else {
+    //   // console.error('Oops! Couldn\'t find user for session:', sessionId);
+    //   // Giving the wheel back to our bot
+    //   return Promise.resolve()
+    // }
   },
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
