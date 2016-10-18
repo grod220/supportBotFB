4cabc896adc4473a6c9f0dd6fe7e781801628229
Gabe Rodriguez <grodriguez@grodriguez-macbookpro3.roam.corp.google.com>
2016-10-18 11:57:06 +0200

    console test

--- app.js
+++ app.js
@@ -104,6 +104,7 @@ const findOrCreateSession = (fbid) => {
 // Our bot actions
 const actions = {
   send({sessionId}, {text}) {
+    console.log(sessionId);
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
     const recipientId = sessions[sessionId].fbid;
